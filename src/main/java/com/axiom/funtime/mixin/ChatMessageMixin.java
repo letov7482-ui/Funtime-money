deeae4c317cc8210de79bb754f823b63a8a59d92
letov7482-ui <letov7482@gmail.com>
2026-07-31 12:54:20 +0300

    Update ChatMessageMixin.java

--- src/main/java/com/axiom/funtime/mixin/ChatMessageMixin.java
+++ src/main/java/com/axiom/funtime/mixin/ChatMessageMixin.java
@@ -1,6 +1,8 @@
+// путь: src/main/java/com/axiom/funtime/mixin/ChatMessageMixin.java
 package com.axiom.funtime.mixin;
 
 import com.axiom.funtime.FunTimeMod;
+import com.axiom.funtime.modules.EventHelper;
 import net.minecraft.client.network.ClientPlayNetworkHandler;
 import net.minecraft.network.packet.s2c.play.GameMessageS2CPacket;
 import org.spongepowered.asm.mixin.Mixin;
@@ -12,8 +14,13 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class ChatMessageMixin {
     @Inject(method = "onGameMessage", at = @At("HEAD"))
     private void onGameMessage(GameMessageS2CPacket packet, CallbackInfo ci) {
+        // Передаём сообщение в AutoTrader
         if (FunTimeMod.CONFIG.autoTraderEnabled && FunTimeMod.autoTrader != null) {
             FunTimeMod.autoTrader.onChatMessage(packet.content());
+        }
+        // Передаём сообщение в EventHelper для парсинга координат событий
+        if (FunTimeMod.CONFIG.eventHelperEnabled) {
+            EventHelper.onChatMessage(packet.content().getString());
         }
     }
 }
