180eb2af0f84b19324c064ef92d5565d83e90560
letov7482-ui <letov7482@gmail.com>
2026-07-31 10:03:31 +0300

    Create ModConfig.java

---
+++ src/main/java/com/axiom/funtime/config/ModConfig.java
@@ -0,0 +1,25 @@
+package com.axiom.funtime.config;
+
+import me.shedaniel.autoconfig.ConfigData;
+import me.shedaniel.autoconfig.annotation.Config;
+import me.shedaniel.autoconfig.annotation.ConfigEntry;
+
+@Config(name = "funtimeubercheat")
+public class ModConfig implements ConfigData {
+    public boolean autoMinerEnabled = true;
+
+    @ConfigEntry.BoundedDiscrete(min = 8, max = 64)
+    public int minerRadius = 32;
+
+    public String priorityOres = "minecraft:ancient_debris,minecraft:diamond_ore,minecraft:emerald_ore";
+    public boolean autoSellToBuyer = true;
+    public boolean comparePrices = true;
+    public int sellReach = 5;
+    public boolean sniperEnabled = true;
+
+    @ConfigEntry.BoundedDiscrete(min = 0.1, max = 0.9)
+    public double sniperProfitThreshold = 0.3;
+
+    public long sniperMaxSpendPerItem = 100_000;
+    public boolean inventoryManagerEnabled = true;
+}
