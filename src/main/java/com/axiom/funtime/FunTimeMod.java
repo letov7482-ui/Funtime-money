439df2e1ecca3ea7d87fc98d1335ed13c4d93183
letov7482-ui <letov7482@gmail.com>
2026-07-31 10:01:42 +0300

    Create FunTimeMod.java

---
+++ src/main/java/com/axiom/funtime/FunTimeMod.java
@@ -0,0 +1,48 @@
+package com.axiom.funtime;
+
+import com.axiom.funtime.config.ModConfig;
+import com.axiom.funtime.modules.*;
+import me.shedaniel.autoconfig.AutoConfig;
+import me.shedaniel.autoconfig.serializer.GsonConfigSerializer;
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import org.lwjgl.glfw.GLFW;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class FunTimeMod implements ModInitializer {
+    public static final Logger LOG = LoggerFactory.getLogger("FunTimeUber");
+    public static ModConfig CONFIG;
+    private AutoMiner autoMiner;
+    private InventoryManager invManager;
+    private MarketSniper sniper;
+
+    @Override
+    public void onInitialize() {
+        LOG.info("FunTime UberCheat starting...");
+        AutoConfig.register(ModConfig.class, GsonConfigSerializer::new);
+        CONFIG = AutoConfig.getConfigHolder(ModConfig.class).getConfig();
+
+        autoMiner = new AutoMiner();
+        invManager = new InventoryManager();
+        sniper = new MarketSniper();
+
+        KeyBinding toggleMinerKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+                "key.funtimeubercheat.autominer", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_M,
+                "category.funtimeubercheat"));
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            while (toggleMinerKey.wasPressed()) {
+                CONFIG.autoMinerEnabled = !CONFIG.autoMinerEnabled;
+                AutoConfig.getConfigHolder(ModConfig.class).save();
+                LOG.info("AutoMiner: {}", CONFIG.autoMinerEnabled ? "ON" : "OFF");
+            }
+            if (client.player == null) return;
+            if (CONFIG.autoMinerEnabled) autoMiner.tick(client);
+            if (CONFIG.inventoryManagerEnabled) invManager.tick(client);
+            if (CONFIG.sniperEnabled) sniper.tick(client);
+        });
+    }
+}
