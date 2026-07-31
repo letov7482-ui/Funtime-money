c9a379a5d13b1fe1b5f6f84bf7388947a4294ca8
letov7482-ui <letov7482@gmail.com>
2026-07-31 10:08:44 +0300

    Create ContainerUtils.java

---
+++ src/main/java/com/axiom/funtime/utils/ContainerUtils.java
@@ -0,0 +1,15 @@
+package com.axiom.funtime.utils;
+
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.util.math.BlockPos;
+
+public class ContainerUtils {
+    public static BlockPos findNearestContainer(MinecraftClient client, int radius) {
+        // scan for chests, shulker boxes, etc.
+        return null;
+    }
+
+    public static void openContainer(BlockPos pos) {
+        // send interaction packet
+    }
+}
