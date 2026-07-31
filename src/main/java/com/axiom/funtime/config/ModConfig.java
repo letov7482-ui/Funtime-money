974311408a836f6430a86bdbdfa68bc490a402d5
letov7482-ui <letov7482@gmail.com>
2026-07-31 11:33:50 +0300

    Update ModConfig.java

--- src/main/java/com/axiom/funtime/config/ModConfig.java
+++ src/main/java/com/axiom/funtime/config/ModConfig.java
@@ -2,30 +2,18 @@ package com.axiom.funtime.config;
 
 import me.shedaniel.autoconfig.ConfigData;
 import me.shedaniel.autoconfig.annotation.Config;
-import me.shedaniel.autoconfig.annotation.ConfigEntry;
 
 @Config(name = "funtimeubercheat")
 public class ModConfig implements ConfigData {
     public boolean autoMinerEnabled = true;
-
-    @ConfigEntry.BoundedDiscrete(min = 8, max = 64)
     public int minerRadius = 32;
-
     public String priorityOres = "minecraft:ancient_debris,minecraft:diamond_ore,minecraft:emerald_ore";
     public boolean autoSellToBuyer = true;
     public boolean comparePrices = true;
-
-    @ConfigEntry.BoundedDiscrete(min = 1, max = 10)
     public int sellReach = 5;
-
     public boolean sniperEnabled = true;
-
-    @ConfigEntry.BoundedDouble(min = 0.1, max = 0.9)   // <-- правильная аннотация
     public double sniperProfitThreshold = 0.3;
-
-    @ConfigEntry.BoundedDiscrete(min = 100, max = 10_000_000)
     public long sniperMaxSpendPerItem = 100_000;
-
     public boolean inventoryManagerEnabled = true;
     public boolean humanSimulatorEnabled = true;
 }
